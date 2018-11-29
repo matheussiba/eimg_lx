5d8b2af9b30446752ffe05636846cf3720a27ab0
Matheus S. Barros <matheus.eco.2010@gmail.com>
2018-11-29 15:23:14 +0100

    git ignore

--- includes/db_credentials server.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db_name = "eimg_lx";
-$host = "localhost";
-$username = "matheus";
-$password = "b7jol9y";
-$port = "5432";
-//Defining PHP variable for the root directory
-$root_directory = "";
-?>
--- includes/db_credentials.php
+++ includes/db_credentials.php
@@ -1,13 +1,4 @@
 <?php
-
-$db_name = "eimg_lx";
-$host = "localhost";
-$username = "matheus";
-$password = "b7jol9y";
-$port = "5432";
-//Defining PHP variable for the root directory
-$root_directory = "";
-
 $db_name = "eimg_lx";
 $host = "localhost";
 $username = "postgres";
