bc6a117763132e696817452834d15f9c6be69665
Matheus S. Barros <matheus.eco.2010@gmail.com>
2018-12-14 12:49:20 +0100

    creating new structure 1.1

--- index.php
+++ index.php
@@ -32,16 +32,6 @@ if (isset($_SESSION['user_id'])) {
 
 
 <body>
-  <script>
-  //  ********* OLD FUNCTIONS
-  var mobileDevice = false;
-  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    /*### DESCRIPTION: Check if the web application is being seen in a mobile device   */
-    mobileDevice = true;
-  };
-
-</script>
-
 <!-- Modal_1 -->
 <div class="modal fade" id="modal_1_intro" tabindex="-1" role="dialog" aria-labelledby="modal_1_intro" aria-hidden="true" data-backdrop="false">
   <div class="modal-dialog modal-lg" role="document">
@@ -99,14 +89,21 @@ if (isset($_SESSION['user_id'])) {
 
         <p><b><h4 id="message_mobile">
           <span class="language-en">
-            Please use your mobile device in the landscape orientation.
+            Please, change the orientation of your device to have a better experience with this application.
           </span>
           <span class="language-pt">
-            Por favor use o seu telemóvel na orientação landscape.
+            Por favor, mude a orientação do seu telemóvel para ter uma melhor experiência com essa aplicação.
           </span>
         </h4></b></p>
 
-        <p><b><h4 id="message_ie"></h4></b></p>
+        <p><b><h4 id="message_ie">
+          <span class="language-en">
+            Please use another browser to access this application, ex: Chrome, Firefox, Safari, Opera.
+          </span>
+          <span class="language-pt">
+            Por favor use outro browser para acessar essa aplicação, ex: Chrome, Firefox, Safari, Opera.
+          </span>
+        </h4></b></p>
 
         <p style="font-size: 12px; margin-top: 30px">
           <span class="language-en">
@@ -181,23 +178,102 @@ if (isset($_SESSION['user_id'])) {
   </div> <!--/.modal-dialog -->
 </div>  <!--/.modal -->
 
+
+<!-- ###############  Div that contains the sidebar ############### -->
+<div id="sidebar_div" class="leaflet-sidebar collapsed">
+  <!-- Nav tabs -->
+  <div id="sidebarTab_div" class="leaflet-sidebar-tabs">
+    <ul id="sidebarTab_top" class="sidebarTab_ul" role="tablist">
+      <li><a href="#home" role="tab"><i class="fa fa-home"></i></a></li>
+      <li><a href="#home" role="tab"><i class="fa fa-thumbs-up"></i></a></li>
+      <li><a href="#home" role="tab"><i class="fa fa-thumbs-down"></i></a></li>
+      <li><a href="#home" role="tab"><i class="fa fa-thumbs-up"></i></a></li>
+      <li><a href="#home" role="tab"><i class="fa fa-thumbs-down"></i></a></li>
+      <li><a href="#home" role="tab"><i class="fa fa-thumbs-down"></i></a></li>
+    </ul>
+    <ul id="sidebarTab_bottom" class="sidebarTab_ul" role="tablist">
+      <li><a href="#info" role="tab"><i class="fa fa-info-circle"></i></a></li>
+    </ul>
+  </div>
+  <!-- Tab panes -->
+  <div class="leaflet-sidebar-content">
+    <div class="leaflet-sidebar-pane" id="home"> </div>
+    <div class="leaflet-sidebar-pane" id="info"> </div>
+  </div>
+</div>
+
+<div id="mapdiv" class="col-md-12"></div>
+
 <script>
-var cookie_lang = getCookie("app_language");
-console.log(cookie_lang);
-if(cookie_lang!=""){
-  $("input[type=radio][name=language_switch][value='"+cookie_lang+"']").prop("checked",true);
-}else{
-  $("input[type=radio][name=language_switch][value='pt']").prop("checked",true);
-}
-// Open the first modal
-$('#modal_1_intro').modal('show');
+var IsMobileDevice, isPortrait, isIE, cookie_lang, checkedValue, minimumZoom, mymap, ctlSidebar, ctlAttribute;
+var LyrAOI, basemap_osm, basemap_mapbox, basemap_Gterrain, basemap_Gimagery, basemap_GimageHybrid, basemap_WorldImagery, Hydda_RoadsAndLabels;
 
-var checkedValue = $('input[type=radio][name=language_switch]:checked').val();
-cbxLangChange(checkedValue);
+$(document).ready(function(){
+  //  ********* Map Initialization *********
+  loadBasemaps();
+  minimumZoom = IsMobileDevice ? 10 : 11;
+  //Create the Leaflet map elemetn
+  mymap = L.map('mapdiv', {
+    center: L.latLng(38.716, -9.150),
+    layers: basemap_mapbox,
+    zoom:14,
+    maxZoom: 18,
+    minZoom: minimumZoom,
+    attributionControl:false,
+    zoomControl:false,
+    // maxBounds: mybounds,
+    maxBoundsViscosity: 1.0
+  });
 
-if (!mobileDevice){
-  document.getElementById("message_mobile").style.display="none";
-}
+  loadStudyArea();
+
+  loadControls(); //Load leaflet controls
+
+}); //END $(document).ready()
+
+
+// mymap.removeLayer(grayscale);
+// mymap.addLayer(streets);
+// basemap_osm
+// basemap_mapbox
+// basemap_Gterrain
+// basemap_Gimagery
+// basemap_GimageHybrid
+// basemap_WorldImagery
+
+
+  // Receive true if the application is being used in Mobile device, false otherwise
+  IsMobileDevice = (((/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) ? true : false);
+  if(IsMobileDevice){
+    if("orientation" in screen) {
+      var orientation_str = screen.orientation.type;
+      var orientation_array = orientation_str.split("-");
+      // Receive true if the application is being used in portrait mode, false otherwise
+      isPortrait = ((orientation_array[0] == "portrait") ? true : false);
+    }
+  }
+  // Receive true if Internet Explorer, false otherwise
+  isIE = ((window.navigator.userAgent.indexOf("MSIE ") > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) ? true : false);
+  console.log(isIE);
+
+  cookie_lang = getCookie("app_language");
+  console.log(cookie_lang);
+  if(cookie_lang!=""){
+    $("input[type=radio][name=language_switch][value='"+cookie_lang+"']").prop("checked",true);
+  }else{
+    $("input[type=radio][name=language_switch][value='pt']").prop("checked",true);
+  }
+  // Open the first modal
+  $('#modal_1_intro').modal('show');
+  checkedValue = $('input[type=radio][name=language_switch]:checked').val();
+  cbxLangChange(checkedValue);
+
+  if (!isPortrait){
+    document.getElementById("message_mobile").style.display="none";
+  }
+  if (!isIE){
+    document.getElementById("message_ie").style.display="none";
+  }
 
 function setCookie(cname, cvalue, exdays) {
   var d = new Date();
@@ -249,10 +325,100 @@ $("#btn_close_modal_intro").on("click", function () {
   }
 
 });
-
 $('input[type=radio][name=language_switch]').change(function() {
   cbxLangChange(this.value);
 });
+
+function loadStudyArea(){
+  /* DESCRIPTION: Adds the Study area, comprises of 12 freguesias:
+   * Estrela, Misericórdia, Santa Maria Maior, São Vicente, Penha de França, Beato,
+   * Arroios, Santo António, Campo de Ourique, Campolide, Avenidas Novas, Areeiro  */
+  $.ajax({
+    url:'map/eimg_get_dbtable.php',
+    data: {
+      type_op: "data",
+      tbl: "study_area_4326",
+      select:"*"
+    },
+    type:'POST',
+    success:function(response){
+      // console.log(response);
+      var layer = JSON.parse(response);
+      // console.log(layer);
+      LyrAOI_coords = layer.features[0].geometry.coordinates;
+      LyrAOI=L.geoJSON(layer);
+      var lyr_bounds = LyrAOI.getBounds();
+      var value = 0.03;
+      LyrAOI.addTo(mymap);
+      //Creating a boundary for the map based on the bounds of the layer added
+      //Increasing lat and long in the same proportion
+      var slt = (lyr_bounds._southWest.lat)-value; //south latitude
+      var sln = (lyr_bounds._southWest.lng)-value*2; //south longitude
+      var nlt = (lyr_bounds._northEast.lat)+value; //north latitude
+      var nln = (lyr_bounds._northEast.lng)+value*2; //north longitude
+      // defining the max bounds for panning around the map
+      var southWest = L.latLng(slt,sln);
+      var northEast = L.latLng(nlt,nln);
+      var mybounds =  L.latLngBounds(southWest, northEast);
+      //Zoom the map to the bounds of the added layer
+      mymap.fitBounds(LyrAOI.getBounds());
+      //Set the maximum boundaries in which the map can be panned
+      mymap.setMaxBounds(mybounds);
+      //Create a test polygon to see the area of the maxBounds
+      // var polygon1 = L.polygon([[slt, sln],[slt, nln],[nlt, nln],[nlt, sln]]).addTo(mymap);
+    },
+    error: function(xhr, status, error){ alert("ERROR: "+error); }
+  }); // End ajax
+}
+function loadBasemaps() {
+  /* DESCRIPTION: Add basemaps to the map*/
+  basemap_osm = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy;<a href="http://osm.org/copyright">OSM</a>'
+  });
+  basemap_mapbox = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2lzMm1hdGhldXMiLCJhIjoiY2lsYXRkcTQ2MGJudXVia25ueXZyMzJkcCJ9.sc74TfXfIWKE2Xw3aVcNvw", {
+    attribution: '&copy;<a href="https://www.mapbox.com/feedback/">Mapbox</a>'
+  });
+  basemap_Gterrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
+    subdomains:['mt0','mt1','mt2','mt3']
+  });
+  basemap_Gimagery = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+    subdomains:['mt0','mt1','mt2','mt3']
+  });
+  basemap_GimageHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
+    subdomains:['mt0','mt1','mt2','mt3']
+  });
+  basemap_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: '&copy;<a href="https://www.esri.com/en-us/home">Esri</a>'
+  });
+  Hydda_RoadsAndLabels = L.tileLayer('https://{s}.tile.openstreetmap.se/hydda/roads_and_labels/{z}/{x}/{y}.png', {
+    name: 'overlay',
+  });
+}
+function loadControls() {
+  /* Add leaflet controls to the map */
+  //Plugin leaflet-sidebar-v2: https://github.com/nickpeihl/leaflet-sidebar-v2
+  ctlSidebar = L.control.sidebar({
+    container:'sidebar_div',
+    autopan: false,
+    closeButton: false,
+  }).addTo(mymap);
+
+  //Add attribution to the map
+  ctlAttribute = L.control.attribution({position:'bottomright'}).addTo(mymap);
+  ctlAttribute.addAttribution('OSM');
+  ctlAttribute.addAttribution('&copy;<a href="http://mastergeotech.info">Master GeoTech</a>');
+  ctlAttribute.addAttribution('&copy;<a href="https://github.com/codeofsumit/leaflet.pm">LeafletPM</a>');
+
+  // ctlLayers = L.control.layers(
+  //   {
+  //     '<i class="fas fa-map-marked"></i>': basemap_mapbox,
+  //     '<i class="fas fa-mountain"></i>': basemap_Gterrain,
+  //     '<i class="fas fa-globe-americas"></i>': basemap_GimageHybrid,
+  //     // '<i class="fas fa-image"></i>': basemap_WorldImagery
+  //   }, null, {collapsed: false}
+  // ).addTo(mymap);
+
+}
 
 </script>
 
